f9d83ef7f59175e16e03052b3dd3aed476e39ffa
FilipSegota <70112731+FilipSegota@users.noreply.github.com>
2022-02-01 11:10:41 -0500

    Add files via upload

---
+++ WarDriver.java
@@ -0,0 +1,22 @@
+/*****************************************************************************
+/* Author:      Filip Segota
+/* Class:       CSC 122, Fall 2020
+/* Assignment:  Lab 4: War
+/* File:        War driver
+/*****************************************************************************/
+import java.util.*;
+
+public class WarDriver{
+    public static void main(String[] args){
+        War game;
+        Scanner input;
+        
+        input = new Scanner(System.in);
+        game = new War();        
+    
+        System.out.println("Welcome to the game of War!\n" +
+                           "Press enter to start.");
+        input.nextLine();
+        game.run();
+    }
+}
